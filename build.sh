aabbad6be3585add46977ce8002e5242d905c26c
Chang Liu <cliu712@build-sb.(none)>
2016-08-26 12:14:50 +1200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cc -Wall -march=native ./find_oscillons.c -o find_oscillons -lm
-cc -Wall -march=native ./extract_profile.c -o extract_profile
+cc -Wall -std=c99 -march=native ./find_oscillons.c -o find_oscillons -lm
+cc -Wall -std=c99 -march=native ./extract_profile.c -o extract_profile
