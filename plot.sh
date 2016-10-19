0fb6646f62a9227d20b537a78d78915b7b9a1efb
Chang Liu <cliu712@aucklanduni.ac.nz>
2016-10-20 10:47:59 +1300

    Normalize y axis

--- plot.sh
+++ plot.sh
@@ -1,7 +1,8 @@
 plot() {
-    gnuplot -e "set terminal png; set output 'plot_$i.png'; plot 'result_$i' using 4 w l;"
+    gnuplot -e "set terminal png; set output 'plot_$1.png'; set xrange [390:1125]; plot 'result_$1' using (column(0)*0.125):(column(4)/0.003989422804014327) w l title 'From simulation'"
 }
 
-for i in $(seq 7); do
-    plot $i
-done
+#for i in $(seq 7); do
+#    plot $i
+#done
+plot 1
