6b3a568476167c5a8d2957e8fa894da7734275df
Chang Liu <cliu712@aucklanduni.ac.nz>
2016-10-30 16:47:32 +1300

    New plots

--- plot.sh
+++ plot.sh
@@ -1,5 +1,5 @@
 plot() {
-    gnuplot -e "set terminal png; set output 'plot_$1.png'; set xrange [390:1125]; plot 'result_$1' using (column(0)*0.125):(column(4)/0.003989422804014327) w l title 'From simulation'"
+    gnuplot -e "set terminal png; set output 'plot_$1.png'; set xrange [390:800]; plot 'result_$1' using (column(0)*0.125):(column(4)/0.003989422804014327) w l title 'From simulation'"
 }
 
 #for i in $(seq 7); do
